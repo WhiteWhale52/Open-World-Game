ba6fc0b32721807916a973d27811c60f5eb2e6d5
JTR781888 <mohamedsama1212@gmail.com>
2024-10-31 23:42:33 -0400

    Added scripts for menu
    Added a settings menu and wrote functions to implement opening and closing the settings and changing the volume of the game.

--- Assets/Scripts 2.0/Main Menu.cs
+++ Assets/Scripts 2.0/Main Menu.cs
@@ -1,10 +1,16 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using UnityEngine.Audio;    
 
 public class MainMenu : MonoBehaviour
 {
+
+    public GameObject settings;
+    public AudioMixer AllSounds;
     public void StartGame()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
@@ -16,8 +22,22 @@ public class MainMenu : MonoBehaviour
         Application.Quit();
     }
 
-    public void Settings()
-    {
-        
+    public void OpenSettings()
+    { 
+       
+        this.gameObject.SetActive(false);
+        settings.SetActive(true);
     }
+
+    public void CloseSettings()
+    {
+        settings.SetActive(false);
+        this.gameObject.SetActive(true);
+    }
+
+    public void SetVolume(float volume)
+    {
+        AllSounds.SetFloat("Volume", volume);
+    }
+
 }
